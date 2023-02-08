505b5c9667da93047d9a75ba129705f9f2a02efb
Federico Salinas <federico.salinassmng@uanl.edu.mx>
2023-02-07 23:28:36 -0600

    Modified files

--- PlottingResultsRoutines/CreatePlots.py
+++ PlottingResultsRoutines/CreatePlots.py
@@ -1,8 +1,8 @@
 from statistics import quantiles
 from matplotlib import use
-from matplotlib.pyplot import figure, rcParams, colorbar
+from matplotlib.pyplot import figure, colorbar
 
-from numpy import pi, arange, linspace, where, histogram, float64, loadtxt, diff, exp
+import numpy as np
 from lmfit.models import GaussianModel
 
 use("TkAgg")
@@ -20,7 +20,7 @@ def CreateResultsFigurePower():
     SubPlot_PowerTime.set_ylabel("TID power")
 
     #Setting time ticks
-    timeTicks = arange(0, 25, 6)
+    timeTicks = np.arange(0, 25, 6)
     SubPlot_PowerTime.set_xlim(0.0, 24.0)
     SubPlot_PowerTime.set_xticks(timeTicks)
 
@@ -54,23 +54,36 @@ def CreateFiguresResults(Coord):
 
     return [(FigureTimeHist, FigurePeriodsHist, Figure_MonthBars), (SubPlot_TimeHist, SubPlot_PeriodHist, SubPlot_MonthsFreq)]
 
-def addTimePowerDataResultsToPlot(Time, Power, Plots, Color, Name, index):
-    #Plotting results
-    if index == 0:
-        Plots[1].plot(Time, Power, "o", color=Color, markersize=2.0, alpha=0.25)
-    else:
-        #Lists to save middle times, mean power and deviation
-        MidTimes = []
-        MeanPerHours = []
-        for i in range(24):
-            MaskTime = where((i <= Time) & (Time <= i+1), True, False)
-            PowerMask = Power[MaskTime]
-            if PowerMask.size  > 0:
-                MeanPower = PowerMask.mean()
+def SavePlot(GenName, RegName, PlotFigure):
+    for format in ["png", "pdf"]:
+        PlotFigure.savefig(f"./../Resultados/{RegName}/{GenName}_{RegName}.{format}")
 
-                MidTimes.append(i + 0.5)
-                MeanPerHours.append(MeanPower)
-        Plots[1].plot(MidTimes, MeanPerHours, "-", color=Color, linewidth=2, markersize=2,label=Name)
+def addTimePowerDataResultsToPlot(Time, Power, Plots, Color, Name, Start):
+    #Plotting mean power with its deviation
+    #Lists to save middle times, mean power and deviation
+    MidTimes = []
+    MeanPerHours = []
+    StdPerHours = []
+    PowerSequences = []
+    for i in range(Start, 24, 4):
+        MaskTime = np.where((i <= Time) & (Time <= i+1), True, False)
+        PowerMask = Power[MaskTime]
+        PowerSequences.append( PowerMask )
+        MidTimes.append(i + 0.5)
+        """
+        if PowerMask.size  > 0:
+            MeanPower = PowerMask.mean()
+            StdPower = PowerMask.std()
+
+            MidTimes.append(i + 0.5)
+            MeanPerHours.append(MeanPower)
+            StdPerHours.append(StdPower)
+
+    Plots[1].errorbar(x=MidTimes, y=MeanPerHours, yerr=StdPerHours, ecolor=Color,
+                    color=Color, elinewidth=2.0, capthick=2.0, capsize=10.0, fmt="o",
+                    label=Name)
+    """
+    Plots[1].boxplot(PowerSequences, positions=MidTimes)
 
 
 def AddTimeMonthsHistogramToPlot(HistogramMonths, absMin, absMax, Plots, Name):
@@ -81,11 +94,11 @@ def AddTimeMonthsHistogramToPlot(HistogramMonths, absMin, absMax, Plots, Name):
     MonthRange = (0, 12)
     #Setting y ticks with months names
     MonthTicks = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    MonthAxisData = linspace(0.5,11.5,12,endpoint=True)
+    MonthAxisData = np.linspace(0.5,11.5,12,endpoint=True)
     Plots[1][0].set_yticks(MonthAxisData, MonthTicks)
 
     extent = (*TimeRange,*MonthRange)
-    timeTicks = arange(0, 25, 6)
+    timeTicks = np.arange(0, 25, 6)
     Plots[1][0].set_xlim(*TimeRange)
     Plots[1][0].set_ylim(*MonthRange)
     Plots[1][0].set_xticks(timeTicks)
@@ -100,9 +113,9 @@ def AddTimeMonthsHistogramToPlot(HistogramMonths, absMin, absMax, Plots, Name):
     elif "SouthEast" in Name:
         TerminatorsFile = "TerminatorHours_SE.dat"
 
-    RiseHours, SetHours = loadtxt(TerminatorsFile, dtype=float64,
+    RiseHours, SetHours = np.loadtxt(TerminatorsFile, dtype=np.float64,
     usecols=(1, 2), unpack=True, skiprows=1)
-    NumMonthTerminator = linspace(0.0, 12.0, RiseHours.size)
+    NumMonthTerminator = np.linspace(0.0, 12.0, RiseHours.size)
 
     HistogramaImagen = Plots[1][0].imshow(HistogramMonths, cmap="cubehelix", interpolation="bessel",
     vmin=absMin, vmax=absMax, aspect="auto", origin="lower", extent=extent)
@@ -110,10 +123,10 @@ def AddTimeMonthsHistogramToPlot(HistogramMonths, absMin, absMax, Plots, Name):
     Plots[1][0].plot(RiseHours, NumMonthTerminator, "--w", linewidth=1.0)
     Plots[1][0].plot(SetHours, NumMonthTerminator, "--w", linewidth=1.0)
 
-    Plots[0][0].savefig(f"./../Resultados/{Name}/OcurrenceTIDs_{Name}.png")
+    SavePlot("OcurrenceTIDs", Name, Plots[0][0])
 
 
-Gaussian_Dist = lambda x, A, sig, mu: (A/(sig*(2.0*pi)**0.5))*exp(-0.5*((x-mu)/sig)**2.0)
+Gaussian_Dist = lambda x, A, sig, mu: (A/(sig*(2.0*np.pi)**0.5))*np.exp(-0.5*((x-mu)/sig)**2.0)
 def AddPeriodHistogramToPlot(Period, Plots, Name):
     #Setting number of bins, period range and also the width of the bars
     Period = 60.0*Period
@@ -121,11 +134,11 @@ def AddPeriodHistogramToPlot(Period, Plots, Name):
     h = 2.0*(Quantiles[2]-Quantiles[0])*(Period.size**(-1/3))
     PeriodRange = (Period.min(), Period.max())
     PeriodBins = int((PeriodRange[1]-PeriodRange[0])/h)
-    BarsPeriod = linspace(PeriodRange[0], PeriodRange[1], PeriodBins)
-    Width = diff(BarsPeriod).mean()
+    BarsPeriod = np.linspace(PeriodRange[0], PeriodRange[1], PeriodBins)
+    Width = np.diff(BarsPeriod).mean()
 
     #Obtaining histogram from period data with given bins and range
-    PeriodHistogram, Edges = histogram(Period, bins=PeriodBins, range=PeriodRange)
+    PeriodHistogram, Edges = np.histogram(Period, bins=PeriodBins, range=PeriodRange)
     #Calculate percentage for each bin
     Ocurrence = 100.0*PeriodHistogram/PeriodHistogram.sum()
 
@@ -146,7 +159,7 @@ def AddPeriodHistogramToPlot(Period, Plots, Name):
     MeanFit, MeanError = ParamsResults["center"].value, ParamsResults["center"].stderr
     SigmaFit, SigmaError = ParamsResults["sigma"].value, ParamsResults["sigma"].stderr
 
-    PRange = linspace(0.0, max(PeriodRange), 100)
+    PRange = np.linspace(0.0, max(PeriodRange), 100)
     labelFit = r"$\mu$={0:.3f}$\pm${1:.3f}".format(MeanFit,MeanError)+"\n"+r"$\sigma$={0:.3f}$\pm${1:.3f}".format(SigmaFit,SigmaError)
 
     Plots[1][1].set_xlim(0.0, max(PeriodRange))
@@ -156,13 +169,12 @@ def AddPeriodHistogramToPlot(Period, Plots, Name):
     label=labelFit)
 
     Plots[1][1].legend()
-    Plots[0][1].savefig(f"./../Resultados/{Name}/PeriodDistribution_{Name}.png")
-
+    SavePlot("PeriodDistribution", Name, Plots[0][1])
 
 def BarsFreq_Month(Time_TIDs, Months_TIDs, Plots, Name):
     #Setting y ticks with months names
     MonthTicks = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    MonthAxisData = linspace(0.5,11.5,12,endpoint=True)
+    MonthAxisData = np.linspace(0.5,11.5,12,endpoint=True)
     Plots[1][2].set_xticks(MonthAxisData, MonthTicks)
 
     #Extract terminator hours
@@ -175,19 +187,18 @@ def BarsFreq_Month(Time_TIDs, Months_TIDs, Plots, Name):
     elif "SouthEast" in Name:
         TerminatorsFile = "TerminatorHours_SE.dat"
 
-    RiseHours, SetHours = loadtxt(TerminatorsFile, dtype=float64,
+    RiseHours, SetHours = np.loadtxt(TerminatorsFile, dtype=np.float64,
     usecols=(1, 2), unpack=True, skiprows=1)
     SizeData = RiseHours.size
     DivH_12 = SizeData//12
     RiseHours, SetHours = RiseHours[0:SizeData:DivH_12], SetHours[0:SizeData:DivH_12]
 
     NumEventerPerMonth = []
-    Months = [i for i in range(1, 13)]
-    for month in Months:
+    for month in range(1,13):
         Conds_month = Months_TIDs==month
         if Conds_month.any():
             Time_Conds_month = Time_TIDs[Conds_month]
-            NumDayNight_month = where((RiseHours[month-1] <= Time_Conds_month) & (Time_Conds_month <= SetHours[month-1]), 1, 0)
+            NumDayNight_month = np.where((RiseHours[month-1] <= Time_Conds_month) & (Time_Conds_month <= SetHours[month-1]), 1, 0)
             NumDay = (NumDayNight_month==1).sum()
             NumNight = (NumDayNight_month==0).sum()
 
@@ -198,10 +209,10 @@ def BarsFreq_Month(Time_TIDs, Months_TIDs, Plots, Name):
     NumEventerPerMonth.sort(key = lambda e: e[0])
     _, NumEvents_Day, NumEvents_Night = zip(*NumEventerPerMonth)
 
-    #Plots[1][3].bar(x=MonthAxisData[:NumBars] - 0.5, height=NumEvents_Day, width=1,
-    #align="edge", edgecolor="k", facecolor="r", label="Day")
-    #Plots[1][3].bar(x=MonthAxisData[:NumBars] - 0.5, height=NumEvents_Night, width=1,
-    #align="edge", edgecolor="k", facecolor="b", label="Night", bottom=NumEvents_Day)
+    Plots[1][2].bar(x=MonthAxisData - 0.25, height=NumEvents_Day, width=0.25,
+    align="edge", edgecolor="k", facecolor="r", label="Day")
+    Plots[1][2].bar(x=MonthAxisData, height=NumEvents_Night, width=0.25,
+    align="edge", edgecolor="k", facecolor="b", label="Night")
 
     Plots[1][2].legend()
-    Plots[0][2].savefig(f"./../Resultados/{Name}/DayNightTIDs_{Name}.png")
+    SavePlot("DayNightTIDs", Name, Plots[0][2])
--- TIDs_ResultsAnalysis.py
+++ TIDs_ResultsAnalysis.py
@@ -3,12 +3,23 @@ from tkinter import Button, Tk
 from os import listdir
 import tkfilebrowser
 from numpy import where, concatenate
+from matplotlib import rcParams
 from matplotlib.pyplot import close, style
-import scienceplots
 
 from DataAnalysisRoutines.GetDataFile import SingleTIDs_Analysis
 from DataAnalysisRoutines.HistogramOcurrence import Time_Months_Ocurrence_Analysis
 from PlottingResultsRoutines.CreatePlots import *
+
+#Ignore events that occoured in dates where a geomagnetic
+#storm had a major effect in the Dst value
+with open("tormentas-2018-2021.txt", "r") as StormDaysData:
+    StormDays = []
+    Lines = StormDaysData.readlines()
+
+    for Line in Lines[1:]:
+        Date = Line.split()[0]
+        if Date not in StormDays:
+            StormDays.append(Line.split()[0])
 
 #Create list LIST_DIRS to save multiple directories
 LIST_DIRS = []
@@ -26,25 +37,9 @@ def StarAnnualAnalysis():
     RESULTS = []
     PowerPlot = CreateResultsFigurePower()
     for dirs in LIST_DIRS:
-        if len(dirs) > 0:
-            ResultsTimeTID = []
-            ResultsPeriodTID = []
-            ResultsPowerTID = []
-            MonthArray = []
+        if len(dirs[0]) > 0:
             ActiveDays = 0
             TotalDays = 0
-
-            #Ignore events that occoured in dates where a geomagnetic
-            #storm had a major effect in the Dst value
-            with open("tormentas-2018-2021.txt", "r") as StormDaysData:
-                StormDays = []
-                Lines = StormDaysData.readlines()
-
-                for Line in Lines[1:]:
-                    Date = Line.split()[0]
-                    if Date not in StormDays:
-                        StormDays.append(Line.split()[0])
-
             #Obtain the full path of the files located in the selected directories and
             #the dates and months of these same files
             for path in dirs[0]:
@@ -56,6 +51,12 @@ def StarAnnualAnalysis():
                 NameOut = path.split("/")[-2]
                 print(f"--Obtaining results of {path}--")
                 TotalDays += len(files_month_full_path)
+                
+                ResultsTimeTID = []
+                ResultsPeriodTID = []
+                ResultsPowerTID = []
+                MonthArray = []
+
                 for fileTID, MonthFile, Date_TID in tqdm(zip(files_month_full_path, MonthPerFile, Dates_TIDs)):
                     if Date_TID not in StormDays:
                         Results = SingleTIDs_Analysis(fileTID)
@@ -68,7 +69,6 @@ def StarAnnualAnalysis():
                             ResultsPowerTID.append(Results[6])
 
             MonthArray = concatenate(tuple(MonthArray), dtype=int)
-
             ResultsTimeTID = concatenate(tuple(ResultsTimeTID))
             #Get the timezone given NameOut
             if "NorthWest" in NameOut:
@@ -79,7 +79,7 @@ def StarAnnualAnalysis():
                 TimeZone = -5.0
             #Apply timezone to get correct Local Time Hours
             ResultsTimeTID += TimeZone
-            ResultsTimeTID = where(ResultsTimeTID<0, ResultsTimeTID+24.0, ResultsTimeTID)
+            ResultsTimeTID = where(ResultsTimeTID < 0, ResultsTimeTID + 24.0, ResultsTimeTID)
 
             ResultsPeriodTID = concatenate(tuple(ResultsPeriodTID))
             ResultsPowerTID = concatenate(tuple(ResultsPowerTID))
@@ -101,25 +101,28 @@ def StarAnnualAnalysis():
             Coord = "(25,25°, -99.9°)"
             NamePower = "LNIG-MNIG"
             ColorPower = "red"
+            RegIndex = 0
         elif "SouthWest" in NamePlot:
             Coord = "(19,81°, -101,69°)"
             NamePower = "UCOE"
             ColorPower = "green"
+            RegIndex = 1
         elif "NorthWest" in NamePlot:
             Coord = "(20,15°, -87,4°)"
             NamePower = "PTEX"
             ColorPower = "blue"
+            RegIndex = 2
         elif "SouthEast" in NamePlot:
             Coord = "(32,28°, -116,52°)"
             NamePower = "CN24-TGMX"
             ColorPower = "black"
+            RegIndex = 3
 
         #Start creating Matplotlib plot to visualize the statistics given the data from RESULTS
         PlotsResults = CreateFiguresResults(Coord)
         AddTimeMonthsHistogramToPlot(DataResults[2], MIN, MAX, PlotsResults, NamePlot)
         AddPeriodHistogramToPlot(DataResults[3], PlotsResults, NamePlot)
-        addTimePowerDataResultsToPlot(DataResults[0], DataResults[4], PowerPlot, ColorPower, NamePower, 0)
-        addTimePowerDataResultsToPlot(DataResults[0], DataResults[4], PowerPlot, ColorPower, NamePower, 1)
+        addTimePowerDataResultsToPlot(DataResults[0], DataResults[4], PowerPlot, ColorPower, NamePower, RegIndex)
         BarsFreq_Month(DataResults[0], DataResults[1], PlotsResults, NamePlot)
         for i in range(2,5):
             close(i)
@@ -134,7 +137,8 @@ def StarAnnualAnalysis():
 
 if __name__=="__main__":
     # Setting plotting format for all figures
-    style.use("science")
+    rcParams["font.family"] = "serif"
+    rcParams["savefig.dpi"] = 400
 
     #Create Tkinter app to select and extract data from TIDs
     #identified in time series of dTEC given .Cmn files
