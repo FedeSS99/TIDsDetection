6591533e95b33d1d24c5d65fdbe3ec8cb47e0eeb
Federico Salinas <federico.salinassmng@uanl.edu.mx>
2023-04-10 17:26:26 -0600

    Edit Main file

--- TIDs_ResultsAnalysis.py
+++ TIDs_ResultsAnalysis.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, mkdir
 from os.path import isdir
 from numpy import where, concatenate
 from matplotlib import rcParams
@@ -7,6 +7,30 @@ from matplotlib.pyplot import close
 from DataScripts.GetDataFile import SingleTIDs_Analysis
 from DataScripts.HistogramOcurrence import Time_Months_Ocurrence_Analysis
 from PlottingScripts.CreatePlots import *
+
+
+#Start creating Matplotlib plot to visualize the statistics given the data from DataDict
+def CreateAnalysisPlots(RegionName, StationName, Stat_or_Reg, DataDict, MIN_OCC, MAX_OCC):
+    if Stat_or_Reg == "Stat":
+        PlotsResults = CreateFiguresResults(StationName, Stat_or_Reg)
+    elif Stat_or_Reg == "Reg":
+        PlotsResults = CreateFiguresResults(RegionName, Stat_or_Reg)
+
+    Add_TimeMonthsHistogramToPlot(DataDict["OCURRENCE"], MIN_OCC, MAX_OCC, PlotsResults, 
+                                  RegionName, StationName, Stat_or_Reg)
+
+    Add_PeriodHistogramToPlot(DataDict["PERIOD"], DataDict["TIME"], DataDict["MONTH"], 
+                              PlotsResults, RegionName, StationName, Stat_or_Reg)
+    
+    Add_BarsFreq_Month(DataDict["TIME"], DataDict["MONTH"], PlotsResults, RegionName, StationName, Stat_or_Reg)
+
+    Add_AmplitudesAnalysis(DataDict["MIN_AMPS"], DataDict["MAX_AMPS"],
+                            DataDict["TIME"], DataDict["MONTH"], PlotsResults, 
+                            RegionName, StationName, Stat_or_Reg)
+    
+
+    for i in range(2,6):
+        close(i)
 
 #---------------------------------------------------------------------------
 def StarAnnualAnalysis(DICT_REGION_STATIONS):
@@ -31,18 +55,29 @@ def StarAnnualAnalysis(DICT_REGION_STATIONS):
         NameOut = DICT_REGION_STATIONS[Region]["ResultPath"].split("/")[-1]
         print(f"-- Working on {NameOut} region --")
         
+        # Declare counter for total days and active days for each region and
+        # also the lists to save the data for all stations
         ActiveDays = 0
         TotalDays = 0
-        ResultsTimeTID = []
-        ResultsPeriodTID = []
-        ResultsPowerTID = []
-        ResultsMinAmps = []
-        ResultsMaxAmps = []
-        MonthArray = []
+        Region_TimeTID = []
+        Region_PeriodTID = []
+        Region_PowerTID = []
+        Region_MinAmps = []
+        Region_MaxAmps = []
+        Region_MonthArray = []
 
         #Obtain the full path of the files located in each station given the Region
         StationsByRegion = DICT_REGION_STATIONS[Region]["DataPaths"].keys()
         for Station in StationsByRegion:
+
+            # Declare the lists to save the data for each station and compute
+            # the statistics for it
+            Station_TimeTID = []
+            Station_PeriodTID = []
+            Station_PowerTID = []
+            Station_MinAmps = []
+            Station_MaxAmps = []
+            Station_MonthArray = []
 
             # Extract DataPaths for each Station and also date and month data
             TIDs_DataPaths = DICT_REGION_STATIONS[Region]["DataPaths"][Station]
@@ -53,78 +88,124 @@ def StarAnnualAnalysis(DICT_REGION_STATIONS):
             for fileTID, MonthFile, Date_TID in zip(TIDs_DataPaths, MonthPerFile, Dates_TIDs):
                 if Date_TID not in StormDays:
                     Results = SingleTIDs_Analysis(fileTID)
-                    SizeResults = Results[0].size
+                    SizeResults = Results["TIME"].size
                     if SizeResults:
                         ActiveDays += 1
 
-                        MonthArray.append(SizeResults*[MonthFile])
-                        ResultsTimeTID.append(Results[0])
-                        ResultsPeriodTID.append(Results[1])
-                        ResultsPowerTID.append(Results[2])
-                        ResultsMinAmps.append(Results[3])
-                        ResultsMaxAmps.append(Results[4])
+                        Region_MonthArray.append(SizeResults*[MonthFile])
+                        Station_MonthArray.append(SizeResults*[MonthFile])
 
-        MonthArray = concatenate(tuple(MonthArray), dtype=int)
-        ResultsTimeTID = concatenate(tuple(ResultsTimeTID))
-        #Get the timezone given NameOut
-        if NameOut == "Center":
-            TimeZone = -6.0
-        elif NameOut == "North":
-            TimeZone = -8.0
-        elif NameOut == "South":
-            TimeZone = -5.0
-        #Apply timezone to get correct Local Time Hours
-        ResultsTimeTID += TimeZone
-        ResultsTimeTID = where(ResultsTimeTID < 0, ResultsTimeTID + 24.0, ResultsTimeTID)
+                        #Get the timezone given NameOut
+                        if NameOut == "North":
+                            TimeZone = -8.0
+                        elif NameOut == "Center":
+                            TimeZone = -6.0
+                        elif NameOut == "South":
+                            TimeZone = -5.0
 
-        ResultsPeriodTID = concatenate(tuple(ResultsPeriodTID))
-        ResultsPowerTID = concatenate(tuple(ResultsPowerTID))
-        ResultsMinAmps = concatenate(tuple(ResultsMinAmps))
-        ResultsMaxAmps = concatenate(tuple(ResultsMaxAmps))
-        NumTIDs = ResultsTimeTID.size
+                        #Apply timezone to get correct Local Time Hours
+                        Results["TIME"] += TimeZone
+                        Results["TIME"] = where(Results["TIME"] < 0, Results["TIME"] + 24.0, Results["TIME"])
+                        Region_TimeTID.append(Results["TIME"])
+                        Station_TimeTID.append(Results["TIME"])
+
+                        Region_PeriodTID.append(Results["PERIOD"])
+                        Station_PeriodTID.append(Results["PERIOD"])
+
+                        Region_PowerTID.append(Results["POWER"])
+                        Station_PowerTID.append(Results["POWER"])
+                        
+                        Region_MinAmps.append(Results["MIN_AMPS"])
+                        Station_MinAmps.append(Results["MIN_AMPS"])
+                        
+                        Region_MaxAmps.append(Results["MAX_AMPS"])
+                        Station_MaxAmps.append(Results["MAX_AMPS"])
+
+
+            Station_MonthArray = concatenate(tuple(Station_MonthArray), dtype=int)
+            Station_TimeTID = concatenate(tuple(Station_TimeTID))
+            Station_PeriodTID = concatenate(tuple(Station_PeriodTID))
+            Station_PowerTID = concatenate(tuple(Station_PowerTID))
+            Station_MinAmps = concatenate(tuple(Station_MinAmps))
+            Station_MaxAmps = concatenate(tuple(Station_MaxAmps))
+
+            StationOcurrenceMap = Time_Months_Ocurrence_Analysis(Station_TimeTID, Station_MonthArray)
+
+            StationResultsDict = {
+                "TIME":Station_TimeTID,
+                "MONTH":Station_MonthArray,
+                "OCURRENCE":StationOcurrenceMap,
+                "PERIOD":Station_PeriodTID,
+                "POWER":Station_PowerTID,
+                "MIN_AMPS":Station_MinAmps,
+                "MAX_AMPS":Station_MaxAmps,
+            }
+
+            StationSavedir = f"./../Results/{Region}/{Station}/"
+            if not isdir(StationSavedir):
+                mkdir(StationSavedir)
+
+            MIN_OCC, MAX_OCC = StationResultsDict["OCURRENCE"].min(), StationResultsDict["OCURRENCE"].max()
+            CreateAnalysisPlots(Region, Station, "Stat", StationResultsDict, MIN_OCC, MAX_OCC)
+            
+
+        # Join all stations data from the same region in single arrays
+        Region_MonthArray = concatenate(tuple(Region_MonthArray), dtype=int)
+        Region_TimeTID = concatenate(tuple(Region_TimeTID))
+        Region_PeriodTID = concatenate(tuple(Region_PeriodTID))
+        Region_PowerTID = concatenate(tuple(Region_PowerTID))
+        Region_MinAmps = concatenate(tuple(Region_MinAmps))
+        Region_MaxAmps = concatenate(tuple(Region_MaxAmps))
+        NumTIDs = Region_TimeTID.size
 
         #Get 2D histogram of TIDs' ocurrence and save all the data from the respective directory
         #in RESULTS
-        HistogramOcurrence = Time_Months_Ocurrence_Analysis(ResultsTimeTID, MonthArray)
-        RESULTS.append([ResultsTimeTID, MonthArray, HistogramOcurrence, ResultsPeriodTID, ResultsPowerTID, ResultsMinAmps, ResultsMaxAmps, NameOut])
+        HistogramOcurrence = Time_Months_Ocurrence_Analysis(Region_TimeTID, Region_MonthArray)
 
-        print(f"No. of TIDs:{NumTIDs}\nActive Days:{ActiveDays} Total Days:{TotalDays}\n")
+        RegionResultsDict = {
+            "TIME":Region_TimeTID,
+            "MONTH":Region_MonthArray,
+            "OCURRENCE":HistogramOcurrence,
+            "PERIOD":Region_PeriodTID,
+            "POWER":Region_PowerTID,
+            "MIN_AMPS":Region_MinAmps,
+            "MAX_AMPS":Region_MaxAmps,
+            "NAME":NameOut
+        }
+
+        RESULTS.append(RegionResultsDict)
+
+        print(f"Total Days:{TotalDays}\nNo. of TIDs:{NumTIDs}\nActive Days:{ActiveDays}\n")
 
     #Obtain the globam minimum and maximum of the ocurrence arrays of all the directories' data
-    MIN, MAX = min([DataResults[2].min() for DataResults in RESULTS]), max([DataResults[2].max() for DataResults in RESULTS])
-    for DataResults in RESULTS:
+    MIN_OCC = min([DataResults["OCURRENCE"].min() for DataResults in RESULTS])
+    MAX_OCC = max([DataResults["OCURRENCE"].max() for DataResults in RESULTS])
+    for RegionDataResults in RESULTS:
         #Get a string Coord to use in the analysis' plots results
-        NamePlot = DataResults[-1]
+        NamePlot = RegionDataResults["NAME"]
         
-        if NamePlot == "Center":
-            NamePower = "MNIG-UCOE"
-            ColorPower = "red"
-            RegIndex = 0
-        elif NamePlot == "North":
+        if NamePlot == "North":
             NamePower = "PTEX-PALX"
             ColorPower = "green"
+            RegIndex = 0
+        elif NamePlot == "Center":
+            NamePower = "MNIG-UCOE"
+            ColorPower = "red"
             RegIndex = 1
         elif NamePlot == "South":
             NamePower = "CN24-UNPM"
             ColorPower = "blue"
             RegIndex = 2
 
-        #Start creating Matplotlib plot to visualize the statistics given the data from RESULTS
-        PlotsResults = CreateFiguresResults(NamePlot)
-        Add_TimeMonthsHistogramToPlot(DataResults[2], MIN, MAX, PlotsResults, NamePlot)
-        Add_PeriodHistogramToPlot(DataResults[3], DataResults[0], DataResults[1], PlotsResults, NamePlot)
-        BoxPlotObject = Add_TimePowerDataResultsToPlot(DataResults[0], DataResults[4], PowerPlot, ColorPower, RegIndex)
+        CreateAnalysisPlots(NamePlot, "", "Reg", RegionDataResults, MIN_OCC, MAX_OCC)
+
+        BoxPlotObject = Add_TimePowerDataResultsToPlot(RegionDataResults["TIME"], RegionDataResults["POWER"], PowerPlot, ColorPower, RegIndex)
         ListBoxPlots.append(BoxPlotObject)
         ListLegendsBoxPlots.append(NamePower)
-
-        Add_BarsFreq_Month(DataResults[0], DataResults[1], PlotsResults, NamePlot)
-        Add_AmplitudesAnalysis(DataResults[5], DataResults[6], DataResults[0], DataResults[1], PlotsResults, NamePlot)
-        for i in range(2,6):
-            close(i)
     
     PowerPlot[1].set_yscale("log", subs=None)
     PowerPlot[0].legend(ListBoxPlots, ListLegendsBoxPlots, loc="upper right")
-    SavePlot("PowerDistributionStations", "", PowerPlot[0])
+    SaveRegionPlot("PowerDistributionStations", "", PowerPlot[0])
     PowerPlot[0].savefig(f"./../Results/PowerDistributionStations.png")
     close(1)
 #---------------------------------------------------------------------------
