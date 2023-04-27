8adec15170ddbf7fe934846cfa8ca3b7700c2b44
Federico Salinas <federico.salinassmng@uanl.edu.mx>
2023-04-27 16:29:03 -0600

    Edit ocurrence map routine

--- DataScripts/HistogramOcurrence.py
+++ DataScripts/HistogramOcurrence.py
@@ -2,22 +2,22 @@ from numpy import histogram, zeros, float32
 
 
 def Time_Months_Ocurrence_Analysis(Time, Months_TIDs):
-    #Setting number of bins and time range for histogram
+    # Setting number of bins and time range for histogram
     TimeBins = 24
     TimeRange = (0.0, 24.0)
     MonthBins = 12
 
-    #Numpy array to save the histogram data for each month per year
-    HistogramMonths = zeros((MonthBins,TimeBins), dtype=float32)
+    # Numpy array to save the histogram data for each month per year
+    HistogramMonths = zeros((MonthBins, TimeBins), dtype=float32)
     setMonths = set(Months_TIDs)
 
     for Month in setMonths:
-        MaskMonths = Months_TIDs==Month
+        MaskMonths = Months_TIDs == Month
         TimeByMonth = Time[MaskMonths]
         index = Month - 1
 
         TimeHistogramByMonth, _ = histogram(TimeByMonth, bins=TimeBins, range=TimeRange)
-        TimeHistogramByMonth = 100*TimeHistogramByMonth/TimeHistogramByMonth.sum()
-        HistogramMonths[index,:] = TimeHistogramByMonth[:]
+        HistogramMonths[index, :] = 100.0*(TimeHistogramByMonth[:]/TimeHistogramByMonth.sum())
+
 
     return HistogramMonths
