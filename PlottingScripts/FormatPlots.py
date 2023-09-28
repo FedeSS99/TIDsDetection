98cc7ce82991d4b0a6d096590d7ed7902f27d98f
Federico Salinas <salinasfederico773@gmail.com>
2023-09-28 12:22:04 -0600

    Changed format for ocurrence and power map plots

--- PlottingScripts/FormatPlots.py
+++ PlottingScripts/FormatPlots.py
@@ -17,56 +17,56 @@ def FormatAndSave_AllRegionPlots(Nplots, PLOTS):
     # Set the limits for Local Time and indexes for each Month
     timeTicks = arange(0, 25, 3)
 
-    # Desactivate UserWarning, expected output
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        PLOTS["OCURR"][0].tight_layout()
-    for p in range(Nplots):
-        PLOTS["OCURR"][1][p].set_yticks(MonthTicksOcurr, MonthStrTicks)
+    for QUANT_VAR, NAME_VAR in zip(["OCURR", "POWER-MED"], ["OcurrenceTIDs", "PowerMedian"]):
+        # Desactivate UserWarning, expected output
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            PLOTS[QUANT_VAR][0].tight_layout()
+        for p in range(Nplots):
+            PLOTS[QUANT_VAR][1][p].set_yticks(MonthTicksOcurr, MonthStrTicks)
 
-        PLOTS["OCURR"][1][p].set_xlim(*TimeRange)
-        PLOTS["OCURR"][1][p].set_ylim(*MonthRange)
-        PLOTS["OCURR"][1][p].set_xticks(timeTicks)
+            PLOTS[QUANT_VAR][1][p].set_xlim(*TimeRange)
+            PLOTS[QUANT_VAR][1][p].set_ylim(*MonthRange)
+            PLOTS[QUANT_VAR][1][p].set_xticks(timeTicks)
 
-        SubplotBox = PLOTS["OCURR"][1][p].get_position()
-        PLOTS["OCURR"][1][p].set_position([SubplotBox.x0, SubplotBox.y0,
-                               0.75*SubplotBox.width, SubplotBox.height])
-    SaveAllRegionPlot("OcurrenceTIDs", PLOTS["OCURR"][0])
+            SubplotBox = PLOTS[QUANT_VAR][1][p].get_position()
+            PLOTS[QUANT_VAR][1][p].set_position([SubplotBox.x0, SubplotBox.y0, 0.75*SubplotBox.width, SubplotBox.height])
+        SaveAllRegionPlot(NAME_VAR, PLOTS[QUANT_VAR][0])
 
     # ------ APPLY FORMAT TO PERIOD DISTRIBUTION FIGURE ------
     PLOTS["PERIOD"][0].tight_layout()
-    MinDistPer_Plots, MaxDistPer_Plots = [], []
-    for p in range(Nplots):      
-        Y_MIN, Y_MAX = PLOTS["PERIOD"][1][p].get_ylim()
-        MinDistPer_Plots.append(Y_MIN)
-        MaxDistPer_Plots.append(Y_MAX)
+    #MinDistPer_Plots, MaxDistPer_Plots = [], []
+    #for p in range(Nplots):      
+    #    Y_MIN, Y_MAX = PLOTS["PERIOD"][1][p].get_ylim()
+    #    MinDistPer_Plots.append(Y_MIN)
+    #    MaxDistPer_Plots.append(Y_MAX)
 
-    Y_MIN, Y_MAX = min(MinDistPer_Plots), max(MaxDistPer_Plots)
-    for p in range(Nplots):
-        PLOTS["PERIOD"][1][p].set_ylim(Y_MIN, Y_MAX)
+    #Y_MIN, Y_MAX = min(MinDistPer_Plots), max(MaxDistPer_Plots)
+    #for p in range(Nplots):
+    #    PLOTS["PERIOD"][1][p].set_ylim(Y_MIN, Y_MAX)
 
     SaveAllRegionPlot("PeriodDistribution", PLOTS["PERIOD"][0])
 
     # ------ APPLY FORMAT TO DAY-NIGHT BARS FIGURE ------
-    PLOTS["DAY-NIGHT_BARS"][0].tight_layout()
+    PLOTS["DAY-NIGHT-BARS"][0].tight_layout()
     MinBar_Plots, MaxBar_Plots = [], []
     for p in range(Nplots):
-        SubplotBox = PLOTS["DAY-NIGHT_BARS"][1][p].get_position()
-        PLOTS["DAY-NIGHT_BARS"][1][p].set_position([SubplotBox.x0, SubplotBox.y0,
+        SubplotBox = PLOTS["DAY-NIGHT-BARS"][1][p].get_position()
+        PLOTS["DAY-NIGHT-BARS"][1][p].set_position([SubplotBox.x0, SubplotBox.y0,
                                                     0.8*SubplotBox.width, SubplotBox.height])
 
-        Y_MIN, Y_MAX = PLOTS["DAY-NIGHT_BARS"][1][p].get_ylim()
+        Y_MIN, Y_MAX = PLOTS["DAY-NIGHT-BARS"][1][p].get_ylim()
         MinBar_Plots.append(Y_MIN)
         MaxBar_Plots.append(Y_MAX)
 
     Y_MIN, Y_MAX = min(MinBar_Plots), max(MaxBar_Plots)
     for p in range(Nplots):
-        PLOTS["DAY-NIGHT_BARS"][1][p].set_ylim(Y_MIN, Y_MAX)
+        PLOTS["DAY-NIGHT-BARS"][1][p].set_ylim(Y_MIN, Y_MAX)
 
-    SaveAllRegionPlot("DayNightTIDs", PLOTS["DAY-NIGHT_BARS"][0])
+    SaveAllRegionPlot("DayNightTIDs", PLOTS["DAY-NIGHT-BARS"][0])
 
     # ------ APPLY FORMAT TO AMPLITUDE AND POWER VARIABILITY FIGURES ------
-    for QUANT_VAR, NAME_VAR in zip(["POWER_VAR", "AMP_VAR"], ["Power", "Amplitude"]):
+    for QUANT_VAR, NAME_VAR in zip(["POWER-VAR", "AMP-VAR"], ["Power", "Amplitude"]):
         # Create Hours and Months ticks to use change the labels in Amplitude variance
         # plots and save the figure
         HourTicks = list(range(0, 25, 4))
@@ -131,34 +131,34 @@ def FormatAndSave_AllRegionPlots(Nplots, PLOTS):
 
 
     # ------ APPLY FORMAT TO AMPLITUDE VS POWER FIGURE ------
-    PLOTS["AMP_POWER"][0].tight_layout()
+    PLOTS["AMP-POWER"][0].tight_layout()
 
     # Apply logaritmic scale to x-axis and y-axis in each Amplitude-Power plot
     # and also set the same y-axis limits in all Amplitude-Power plots
     MinAmpPow_Plots, MaxAmpPow_Plots = [], []
     for p in range(Nplots):
-        SubplotBox = PLOTS["AMP_POWER"][1][p].get_position()
-        PLOTS["AMP_POWER"][1][p].set_position([1.15*SubplotBox.x0, SubplotBox.y0,
+        SubplotBox = PLOTS["AMP-POWER"][1][p].get_position()
+        PLOTS["AMP-POWER"][1][p].set_position([1.15*SubplotBox.x0, SubplotBox.y0,
                                                0.7*SubplotBox.width, SubplotBox.height])
 
-        Y_MIN, Y_MAX = PLOTS["AMP_POWER"][1][p].get_ylim()
+        Y_MIN, Y_MAX = PLOTS["AMP-POWER"][1][p].get_ylim()
         MinAmpPow_Plots.append(Y_MIN)
         MaxAmpPow_Plots.append(Y_MAX)
-        PLOTS["AMP_POWER"][1][p].set_yscale("log", subs=None)
+        PLOTS["AMP-POWER"][1][p].set_yscale("log", subs=None)
 
-    PLOTS["AMP_POWER"][1][Nplots-1].set_xscale("log", subs=None)
+    PLOTS["AMP-POWER"][1][Nplots-1].set_xscale("log", subs=None)
 
     Y_MIN, Y_MAX = min(MinAmpPow_Plots), max(MaxAmpPow_Plots)
     for p in range(Nplots):
         # Desactivate UserWarning, expected output
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            PLOTS["AMP_POWER"][1][p].set_ylim(Y_MIN, Y_MAX)
+            PLOTS["AMP-POWER"][1][p].set_ylim(Y_MIN, Y_MAX)
 
     # Fixing position of legends box outside the subplots
-    PLOTS["AMP_POWER"][0].legend(loc="center right", bbox_to_anchor=(1, 0.5),
+    PLOTS["AMP-POWER"][0].legend(loc="center right", bbox_to_anchor=(1, 0.5),
                                  fancybox=True, shadow=True)
 
-    SaveAllRegionPlot("AmplitudePowerRegions", PLOTS["AMP_POWER"][0])
+    SaveAllRegionPlot("AmplitudePowerRegions", PLOTS["AMP-POWER"][0])
 
     print("finished!")
