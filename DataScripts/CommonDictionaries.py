4b08ef599e8b0882a0a61cecb0b8bb0201ecb6d3
Federico Salinas <salinasfederico773@gmail.com>
2024-06-29 00:34:59 -0600

    Update regions names and terminator files in dicts

--- DataScripts/CommonDictionaries.py
+++ DataScripts/CommonDictionaries.py
@@ -1,9 +1,9 @@
 # Dictionary to extract filename of Terminator data for each region
 TerminatorsDict = {
-    "North":"./TerminatorData/TerminatorHours_North.dat",
-    "Center-MNIG":"./TerminatorData/TerminatorHours_Center.dat",
-    "Center-UCOE":"./TerminatorData/TerminatorHours_Center.dat",
-    "South":"./TerminatorData/TerminatorHours_South.dat"
+    "West":"./TerminatorData/TerminatorHours_West.dat",
+    "Center-MNIG":"./TerminatorData/TerminatorHours_Center-MNIG.dat",
+    "Center-UCOE":"./TerminatorData/TerminatorHours_Center-UCOE.dat",
+    "East":"./TerminatorData/TerminatorHours_East.dat"
     }
 
 # Dictionary to extract colors to use in day-night filter for amplitude-power data
@@ -11,8 +11,8 @@ DayNightColors = dict(Day="red", Night="blue")
 
 # Dictionary to assign title to each region subplot
 IndexName = {
-    0: "A) North",
+    0: "A) West",
     1: "B) Center-MNIG",
     2: "C) Center-UCOE",
-    3: "D) South"
+    3: "D) East"
     }
--- TIDs_StatsAnalysis.py
+++ TIDs_StatsAnalysis.py
@@ -5,16 +5,16 @@ if __name__ == "__main__":
     DATA_COMMON_PATH = "../Analysis/"
     RESULTS_COMMON_PATH = "../Results/"
 
-    SUBDIRECTORIES_REGIONS = ["North", "Center-MNIG", "Center-UCOE", "South"]
+    SUBDIRECTORIES_REGIONS = ["West", "Center-MNIG", "Center-UCOE", "East"]
 
     # Create dictionary for atributes to use as input information for each Region
     # plot; the information has to be given in the following order
     # [ Color, Symbol marker, Index, Timezone]
     RegionsInfo = {
-        "North": ["blue", "^", 0, -8.0],
+        "West": ["blue", "^", 0, -8.0],
         "Center-MNIG": ["green", "*", 1, -6.0],
         "Center-UCOE": ["lime", "o", 2, -6.0],
-        "South": ["red", "s", 3, -5.0]
+        "East": ["red", "s", 3, -5.0]
     }
 
     InputRegionsData = CreateInputDictionary(
